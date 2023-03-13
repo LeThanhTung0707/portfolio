d640ce9c02d28f97b52e58dc5aeac37e0a977325
LeThanhTung0707 <tenanhlavip@gmail.com>
2023-03-13 15:15:09 +0700

    Final - 1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="flex justify-center backdrop-blur-sm fixed w-full">
-    <div class="flex flex-row justify-between items-center w-full max-w-[1344px]">
+    <div class="flex flex-row justify-between items-center w-full max-w-[1344px] relative">
       <div class="">
         <img class="h-[80px]" src="/images/logo.png" alt="">
       </div>
-      <div class="flex space-x-10 px-10 text-[18px]">
-        <div class="cursor-pointer hover:text-[#79ACD9]">About</div>
-        <div class="cursor-pointer hover:text-[#79ACD9]">Experience</div>
-        <div class="cursor-pointer hover:text-[#79ACD9]">Work</div>
-        <div class="cursor-pointer hover:text-[#79ACD9]">Contact</div>
+      <div class="px-10 h-full flex justify-center items-center md:hidden" @click="() => {hiddenMenu = !hiddenMenu}"><i class="ri-menu-line"></i></div>
+      <div :class="hiddenMenu ? 'hidden' : 'flex'" class="bg-white backdrop-blur-sm md:bg-transparent md:flex absolute top-[100%] w-full md:relative md:top-0 justify-center items-center md:justify-end  flex-col md:flex-row md:space-x-10 md:px-10 text-[18px]">
+        <NuxtLink to="#about">
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="cursor-pointer hover:text-[#79ACD9]">About</div>
+        </NuxtLink>
+        <NuxtLink to="#experience">
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="cursor-pointer hover:text-[#79ACD9]">Experience</div>
+        </NuxtLink>
+        <NuxtLink to="#work">
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="cursor-pointer hover:text-[#79ACD9]">Work</div>
+        </NuxtLink>
+        <NuxtLink to="#contact">
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="cursor-pointer hover:text-[#79ACD9]">Contact</div>
+        </NuxtLink>
       </div>
     </div>
   </div>
@@ -18,7 +27,12 @@
 </template>
 
 <script setup lang="ts">
-
+const hiddenMenu = ref<boolean>(true)
+onMounted(() => {
+  addEventListener('scroll' , () => {
+    hiddenMenu.value = true
+  })
+})
 </script>
 
 <style scoped>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col justify-center min-h-screen items-center pt-[80px]">
-    <section class="max-w-[1344px] px-[100px] w-full pt-[40px] flex flex-col justify-center items-center">
+    <section class="max-w-[1344px] px-6 md:px-[100px] w-full pt-[40px] flex flex-col justify-center items-center">
       <div class="flex flex-col justify-center items-center py-10 mt-10">
         <div class="flex flex-col justify-start items-start">
-          <div class="font-bold text-[#79ACD9] text-[30px] pl-[4px]">Hi, my name is</div>
-          <div class="text-[48px] font-bold text-[#141C3A]">Lê Thanh Tùng</div>
-          <div class="text-[48px] font-bold text-[#141C3A] mb-6">I'm Frontend & Backend Developer</div>
+          <div class="font-bold text-[#79ACD9] text-[24px] md:text-[30px] pl-[4px]">Hi, my name is</div>
+          <div class="text-[32px] md:text-[48px] font-bold text-[#141C3A]">Lê Thanh Tùng</div>
+          <div class="text-[32px] md:text-[48px] font-bold text-[#141C3A] mb-6">I'm Frontend & Backend Developer</div>
           <div class="mb-6 text-[1.5rem] leadding-[1.5]">I code beautifully simple things, and I love what I do.</div>
         </div>
         <div class="mb-6"><img class="w-[150px] md:w-[210px]" src="/images/avatar.png" alt=""></div>
@@ -14,17 +14,17 @@
         <img src="/images/pick3.svg" alt="">
       </div>
     </section>
-    <section class="bg-[#79ACD9] w-full flex flex-col justify-center items-center pt-40 pb-72 px-20 text-white">
-      <div class="w-[60%] flex items-center justify-center flex-col">
-        <div class="font-bold text-[35px] mb-10">Hi, I’m Tùng. Nice to meet you.</div>
-        <h2 style="word-break: break-word;" class="break-words leading-6  px-8 text-center mb-10">I’m a software engineer specializing in building exceptional digital experiences. I enjoy creating things that live on the internet. My interest in web development started back in 2022 when I took a course on website design. 
+    <section id="about" class="bg-[#79ACD9] w-full flex flex-col justify-center items-center pt-40 pb-72 px-6 md:px-20 text-white">
+      <div class="w-full lg:w-[60%] flex items-center justify-center flex-col">
+        <div class="font-bold text-[24px] md:text-[35px] mb-10 text-center md:text-start">Hi, I’m Tùng. Nice to meet you.</div>
+        <h2 style="word-break: break-word;" class="break-words leading-6  md:px-8 text-center mb-10">I’m a software engineer specializing in building exceptional digital experiences. I enjoy creating things that live on the internet. My interest in web development started back in 2022 when I took a course on website design. 
           I'm quietly confident, naturally curious, and perpetually working on improving my chops one develop problem at a time.
         </h2>
       </div>
     </section>
-    <section class="w-full flex flex-col justify-center items-center mt-[-18rem] mb-12">
-      <div class="max-w-[1000px] grid grid-cols-2 w-full rounded-3xl overflow-hidden border-[1px] shadow-lg">
-        <div class=" bg-white w-full flex flex-col items-center justify-start border-r-[1px]">
+    <section id="experience" class="w-full flex flex-col justify-center items-center mt-[-18rem] mb-12 px-2 lg:px-0">
+      <div class="max-w-[1000px] grid grid-cols-1 md:grid-cols-2 w-full rounded-3xl overflow-hidden border-[1px] shadow-lg">
+        <div class=" bg-white w-full flex flex-col items-center justify-start md:border-r-[1px]">
           <div class="my-20 flex flex-col items-center justify-start">
             <div class="flex items-center justify-center pt-10 pb-4 mt-8">
               <div class="text-white rounded-[50%] bg-[#79ACD9] w-[80px] h-[80px] flex justify-center items-center text-[50px]"><i class="ri-computer-line"></i></div>
@@ -45,7 +45,7 @@
           </div>
         </div>
 
-        <div class=" bg-white w-full flex flex-col items-center justify-start">
+        <div class=" bg-white w-full flex flex-col items-center justify-start border-t-[1px] md:border-none">
           <div class="my-20 flex flex-col items-center justify-start">
             <div class="flex items-center justify-center pt-10 pb-4 mt-8">
               <div class="text-white rounded-[50%] bg-[#79ACD9] w-[80px] h-[80px] flex justify-center items-center text-[50px]"><i class="ri-server-line"></i></div>
@@ -70,19 +70,57 @@
 
       </div>
     </section>
-    <section class="w-full flex justify-center items-center flex-col">
+    <section id="work" class="w-full flex justify-center items-center flex-col pb-10">
       <div class="flex flex-col justify-center items-center max-w-[1344px] w-full">
-        <div class="flex items-center"><span class="mr-6 font-bold text-[35px]">Where I've worked</span><h2 class="w-[300px] h-[1px] bg-black"></h2></div>
-        <div class="flex justify-center items-center w-full px-[100px] py-20">
-          <div class="flex flex-col tab">
-            <div class="border-l-2 p-3 px-10 w-full cursor-pointer hover:bg-sky-100">PanSoft</div>
-            <div class="border-l-2 p-3 px-10 w-full cursor-pointer hover:bg-sky-100">Stech</div>
+        <div class="flex items-center"><span class="mr-6 font-bold text-[24px] md:text-[35px]">Where I've worked</span><h2 class="md:w-[300px] h-[1px] bg-black"></h2></div>
+        <div class="flex flex-col md:flex-row w-full md:w-[70%] py-20 space-x-4">
+          <div class="h-full flex flex-row md:flex-col">
+            <template v-for="(j, ind) in job">
+              <div @click="chooseJob(ind)" :class="ind == currentJob ? 'bg-sky-100 cursor-default border-l-[#79ACD9]' : 'hover:bg-sky-200 cursor-pointer'" class="border-l-2 p-3 px-10 w-full duration-1000">{{ j.name }}</div>
+            </template>
           </div>
-          <div class="content">
-            <div class="w-[300px]">123</div>
+          <div class="h-full mt-4 md:mt-0">
+            <div class="flex flex-col h-full ">
+              <div class="font-bold text-[20px] leading-[1.3] mb-2 flex flex-col lg:flex-row">{{ desJob.role }}  <span class="text-[#79ACD9] lg:ml-2">@ {{ desJob.name }}</span></div>
+              <div class="text-[14px] leading-[1.3] mb-2 opacity-60 font-bold">{{ desJob.duration }}</div>
+              <ul class="pt-5">
+                <template v-for="d in desJob.description">
+                  <li class="py-2 pr-4 lg:pr-0"><span><i class="ri-play-line text-[#79ACD9]"></i></span> {{ d }}</li>
+                </template>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
+    </section>
+    
+    <section id="contact" class="w-full flex justify-center items-center flex-col pb-10 ">
+      <div class="font-bold text-[20px]">What's next ?</div>
+      <div class="font-bold text-[36px] pb-3">Get In Touch</div>
+      <div class="pb-10 text-center lg:text-start">Interested in working together? We should queue up a time to chat. I'll buy the coffee.</div>
+      <a href="mailto:lethanhtung12a1la18@gmail.com?subject=Hello&body=Hello!" class="flex items-center justify-center py-2 px-3 rounded-3xl border-[2px] border-green-500 hover:bg-green-500 hover:text-white cursor-pointer duration-500"><i class="ri-links-line px-1"></i>Let's do this</a>
+    </section>
+    <section class="py-12 bg-[#79ACD9] w-full flex flex-col justify-center items-center">
+      <div class="max-w-[1344px] flex flex-col justify-center items-center">
+        <div class=" pb-10 flex flex-col w-full items-center">
+          <div class="col-span-2  flex flex-col text-white pb-10">
+            <div class="text-white text-[30px] mb-4">Contact Infomation</div>
+            <div><i class="ri-map-pin-2-fill"></i> <span class="font-bold">Address</span> Ho Chi Minh City, Viet Nam</div>
+            <div><i class="ri-phone-fill"></i> <span class="font-bold">Phone</span> +84355780316</div>
+            <div><i class="ri-mail-fill"></i> <span class="font-bold">Mail</span> lethanhtung12a1la18@gmail.com</div>
+            <div class="flex items-center space-x-4 mt-10">
+              <a href="https://www.facebook.com/tung.lethanh.3532507/" target="_blank">
+                <div class="hover:scale-110"><img class="w-14 h-14" title="FACEBOOK" src="/images/contact/facebook.svg" alt=""></div>
+              </a>
+              <a href="https://www.instagram.com/le_ethanhtung_f/" target="_blank">
+                <div class="hover:scale-110"><img class="w-14 h-14" title="INSTAGRAM" src="/images/contact/instagram.svg" alt=""></div>
+              </a>
+            </div>
+          </div>
+          <div class="w-full text-white font-bold text-center text-[24px] px-2 md:px-0">Living, learning, & leveling up one day at a time.</div>
+        </div>
+      </div>
+      
     </section>
   </div>
 </template>
@@ -91,9 +129,32 @@
 const job = [
   {
     name: 'Pansoft',
-    
+    duration: "July 2022 - Present",
+    role: "Frontend & Backend Developer",
+    description: [
+      'Write modern, performant, maintainable code for a diverse array of client projects',
+      'Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Nuxtjs, Nestjs',
+      'Write APIs to connect between systems and serve data exchange with frontend'
+    ]
+  },
+  {
+    name: 'Stech',
+    duration: "February 2022 - June 2022",
+    role: "Backend Developer",
+    description: [
+      'Developed and maintained code for in-house websites primarily using Javascript, TypeScript.',
+      'Manually tested sites in various browsers and mobile devices to ensure cross-browser compatibility and responsiveness.',
+      "Participate in website development (create and update API)",
+    ]
   }
 ]
+const currentJob = ref<number>(0)
+const desJob = computed(() => {
+  return job[currentJob.value]
+})
+const chooseJob = (ind:number) => {
+  if (currentJob.value != ind) currentJob.value = ind;
+}
 </script>
 
 <style scoped>
