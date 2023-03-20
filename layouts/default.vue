ab9bd9eb28a1dba280b91bb3e63f97c820c4470f
LeThanhTung0707 <tenanhlavip@gmail.com>
2023-03-20 17:16:56 +0700

    Change color+ hide/show navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center backdrop-blur-lg fixed w-full z-50">
+  <div id="navbar" class="flex justify-center backdrop-blur-xl fixed w-full z-50 duration-1000">
     <div class="flex flex-row justify-between items-center w-full max-w-[1344px] relative">
       <div class="animate__fadeInDown animate__animated delay1">
         <img class="h-[80px]" src="/images/logo.png" alt="">
@@ -7,16 +7,16 @@
       <div class="px-10 h-full flex justify-center items-center md:hidden" @click="() => {hiddenMenu = !hiddenMenu}"><i class="ri-menu-line"></i></div>
       <div :class="hiddenMenu ? 'hidden' : 'flex'" class=" bg-white backdrop-blur-sm md:bg-transparent md:flex absolute top-[100%] w-full md:relative md:top-0 justify-center items-center md:justify-end  flex-col md:flex-row md:space-x-10 md:px-10 text-[18px]">
         <NuxtLink to="#about">
-          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay2 cursor-pointer hover:text-[#79ACD9]">About</div>
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay2 cursor-pointer hover:text-rose-900">About</div>
         </NuxtLink>
         <NuxtLink to="#experience">
-          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay3 cursor-pointer hover:text-[#79ACD9]">Experience</div>
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay3 cursor-pointer hover:text-rose-900">Experience</div>
         </NuxtLink>
         <NuxtLink to="#work">
-          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay4 cursor-pointer hover:text-[#79ACD9]">Work</div>
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay4 cursor-pointer hover:text-rose-900">Work</div>
         </NuxtLink>
         <NuxtLink to="#contact">
-          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay5 cursor-pointer hover:text-[#79ACD9]">Contact</div>
+          <div @click="() => {hiddenMenu = !hiddenMenu}" class="animate__fadeInDown animate__animated delay5 cursor-pointer hover:text-rose-900">Contact</div>
         </NuxtLink>
       </div>
     </div>
@@ -24,7 +24,7 @@
   <div>
     <slot />
   </div>
-  <div class="mix-blend-difference animate__fadeIn animate__animated delay5 hidden md:flex fixed bottom-0 left-0 items-end ml-7 text-lime-700">
+  <div class="mix-blend-difference animate__fadeIn animate__animated delay5 hidden md:flex fixed bottom-0 left-0 items-end ml-7 text-rose-800">
     <div class="flex flex-col ">
       <a href="https://www.facebook.com/tung.lethanh.3532507/" target="_blank">
         <div class="text-[20px]"><i class="ri-facebook-line"></i></div>
@@ -32,22 +32,38 @@
       <a href="https://www.instagram.com/le_ethanhtung_f/" target="_blank">
         <div class="text-[20px]"><i class="ri-instagram-line"></i></div>
       </a>
-      <div class="w-[1px] h-[90px] bg-lime-700 m-auto"></div>
+      <div class="w-[1px] h-[90px] bg-rose-800 m-auto"></div>
     </div>
   </div>
-  <div orientation="right" class="mix-blend-difference animate__fadeIn animate__animated delay5 hidden md:flex fixed bottom-0 right-0 items-end text-lime-700 mr-3">
+  <div orientation="right" class="mix-blend-difference animate__fadeIn animate__animated delay5 hidden md:flex fixed bottom-0 right-0 items-end mr-3 text-rose-800">
     <div class="flex flex-col">
       <div class="write mb-3">tenanhlavip@gmail.com </div>
-      <div class="w-[1px] h-[90px] bg-lime-700 m-auto"></div>
+      <div class="w-[1px] h-[90px] bg-rose-800 m-auto"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const hiddenMenu = ref<boolean>(true)
+const lastScrollTop = ref<number>(0)
 onMounted(() => {
   addEventListener('scroll' , () => {
-    hiddenMenu.value = true
+    hiddenMenu.value = true;
+    const navbar = document.getElementById('navbar')
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop.value) {
+      navbar.style.top = '-80px'
+    } else {
+      navbar.style.top = '0px'
+    }
+    if( scrollTop != 0 ) {
+      navbar.style.borderBottom = '1px'
+      navbar.style.boxShadow = "-15px 0px 10px -15px #111"
+    } else {
+      navbar.style.borderBottom = '0px'
+      navbar.style.boxShadow = 'none'
+    }
+    lastScrollTop.value = scrollTop
   })
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="delay-first needAnimate max-w-[1344px] px-6 md:px-[100px] w-full pt-[40px] flex flex-col justify-center items-center">
       <div class="flex flex-col justify-center items-center py-10 mt-10">
         <div class="flex flex-col justify-start items-start">
-          <div class="font-bold text-[#79ACD9] text-[24px] md:text-[30px] pl-[4px]">Hi, my name is</div>
+          <div class="font-bold text-[#080808] text-[24px] md:text-[30px] pl-[4px]">Hi, my name is</div>
           <div class="text-[32px] md:text-[48px] font-bold text-[#141C3A]">Lê Thanh Tùng</div>
           <div class="text-[32px] md:text-[48px] font-bold text-[#141C3A] mb-6">I'm Frontend & Backend Developer</div>
           <div class="mb-6 text-[1.5rem] leadding-[1.5]">I code beautifully simple things, and I love what I do.</div>
@@ -14,7 +14,7 @@
         <img src="/images/pick3.svg" alt="">
       </div>
     </section>
-    <section id="about" class="bg-[#79ACD9] w-full flex flex-col justify-center items-center pt-40 pb-72 px-6 md:px-20 text-white">
+    <section id="about" class="bg-[#080808] w-full flex flex-col justify-center items-center pt-40 pb-72 px-6 md:px-20 text-white">
       <div class="needAnimate w-full lg:w-[60%] flex items-center justify-center flex-col">
         <div class="font-bold text-[24px] md:text-[35px] mb-10 text-center md:text-start">Hi, I’m Tùng. Nice to meet you.</div>
         <h2 style="word-break: break-word;" class="break-words leading-6  md:px-8 text-center mb-10">I’m a software engineer specializing in building exceptional digital experiences. I enjoy creating things that live on the internet. My interest in web development started back in 2022 when I took a course on website design. 
@@ -27,13 +27,13 @@
         <div class=" bg-white w-full flex flex-col items-center justify-start md:border-r-[1px]">
           <div class="my-20 flex flex-col items-center justify-start">
             <div class="flex items-center justify-center pt-10 pb-4 mt-8">
-              <div class="text-white rounded-[50%] bg-[#79ACD9] w-[80px] h-[80px] flex justify-center items-center text-[50px]"><i class="ri-computer-line"></i></div>
+              <div class="text-white rounded-[50%] bg-[#080808] w-[80px] h-[80px] flex justify-center items-center text-[50px]"><i class="ri-computer-line"></i></div>
             </div>
             <div class="font-bold pb-5 text-[24px]">
               Frontend Developer
             </div>
             <div class="text-center px-10 h-20">Builds the front-end portion of websites and web applications—the part users see and interact with.</div>
-            <div class="pt-7 pb-4 text-[20px] text-[#79ACD9] font-bold">Skill</div>
+            <div class="pt-7 pb-4 text-[20px] text-[#080808] font-bold">Skill</div>
             <div class="grid grid-cols-4 gap-4 mt-8 ">
               <div class="hover:scale-110 w-[64px] h-[64px] text-[64px] flex items-center text-orange-600" title="HTML"><i class="ri-html5-fill"></i></div>
               <div class="hover:scale-110 w-[64px] h-[64px]"><img class="w-[64px] h-[64px]" title="CSS" src="/images/skill/css.svg"></div>
@@ -48,13 +48,13 @@
         <div class=" bg-white w-full flex flex-col items-center justify-start border-t-[1px] md:border-none">
           <div class="my-20 flex flex-col items-center justify-start">
             <div class="flex items-center justify-center pt-10 pb-4 mt-8">
-              <div class="text-white rounded-[50%] bg-[#79ACD9] w-[80px] h-[80px] flex justify-center items-center text-[50px]"><i class="ri-server-line"></i></div>
+              <div class="text-white rounded-[50%] bg-[#080808] w-[80px] h-[80px] flex justify-center items-center text-[50px]"><i class="ri-server-line"></i></div>
             </div>
             <div class="font-bold pb-5 text-[24px]">
               Backend Developer
             </div>
             <div class="text-center px-10 h-20">Creates code that does relational mapping to retrieve data from a database</div>
-            <div class="pt-7 pb-4 text-[18px] text-[#79ACD9] font-bold">Skill</div>
+            <div class="pt-7 pb-4 text-[18px] text-[#080808] font-bold">Skill</div>
             <div class="grid grid-cols-4 gap-4 mt-8 ">
               <div class="hover:scale-110"><img title="Nodejs" src="/images/skill/nodejs-icon.svg" alt=""></div>
               <div class="hover:scale-110 "><img title="Expressjs" src="/images/skill/expressjs.svg" alt=""></div>
@@ -76,16 +76,16 @@
         <div class="flex flex-col md:flex-row w-full md:w-[70%] py-20 space-x-4">
           <div class="h-full flex flex-row md:flex-col">
             <template v-for="(j, ind) in job">
-              <div @click="chooseJob(ind)" :class="ind == currentJob ? 'bg-sky-100 cursor-default border-l-[#79ACD9]' : 'hover:bg-sky-200 cursor-pointer'" class="border-l-2 p-3 px-10 w-full duration-1000">{{ j.name }}</div>
+              <div @click="chooseJob(ind)" :class="ind == currentJob ? 'bg-sky-100 cursor-default border-l-[#080808]' : 'hover:bg-sky-200 cursor-pointer'" class="border-l-2 p-3 px-10 w-full duration-1000">{{ j.name }}</div>
             </template>
           </div>
           <div class="h-full mt-4 md:mt-0">
             <div class="flex flex-col h-full ">
-              <div class="font-bold text-[20px] leading-[1.3] mb-2 flex flex-col lg:flex-row">{{ desJob.role }}  <span class="text-[#79ACD9] lg:ml-2">@ {{ desJob.name }}</span></div>
+              <div class="font-bold text-[20px] leading-[1.3] mb-2 flex flex-col lg:flex-row">{{ desJob.role }}  <span class="text-[#080808] lg:ml-2">@ {{ desJob.name }}</span></div>
               <div class="text-[14px] leading-[1.3] mb-2 opacity-60 font-bold">{{ desJob.duration }}</div>
               <ul class="pt-5">
                 <template v-for="d in desJob.description">
-                  <li class="py-2 pr-4 lg:pr-0"><span><i class="ri-play-line text-[#79ACD9]"></i></span> {{ d }}</li>
+                  <li class="py-2 pr-4 lg:pr-0"><span><i class="ri-play-line text-[#080808]"></i></span> {{ d }}</li>
                 </template>
               </ul>
             </div>
@@ -100,7 +100,7 @@
       <div class="pb-10 text-center lg:text-start">Interested in working together? We should queue up a time to chat. I'll buy the coffee.</div>
       <a href="mailto:lethanhtung12a1la18@gmail.com?subject=Hello&body=Hello!" class="flex items-center justify-center py-2 px-3 rounded-3xl border-[2px] border-green-500 hover:bg-green-500 hover:text-white cursor-pointer duration-500"><i class="ri-links-line px-1"></i>Let's do this</a>
     </section>
-    <section class="py-12 bg-[#79ACD9] w-full flex flex-col justify-center items-center">
+    <section class="py-12 bg-[#080808] w-full flex flex-col justify-center items-center">
       <div class="max-w-[1344px] flex flex-col justify-center items-center">
         <div class=" pb-10 flex flex-col w-full items-center">
           <div class="needAnimate col-span-2  flex flex-col text-white pb-10">
